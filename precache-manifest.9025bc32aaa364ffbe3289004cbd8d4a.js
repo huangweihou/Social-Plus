bde50ae6341e6982649699e42dd97d51e87521e3
peter <peter@MacBook-Pro.local>
2020-08-26 20:10:12 +0800

    Updates

---
+++ precache-manifest.9025bc32aaa364ffbe3289004cbd8d4a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb304833c877c761a6b55f4db0c4322d",
+    "url": "/Social-Plus/index.html"
+  },
+  {
+    "revision": "0f8e03ee711abb33482a",
+    "url": "/Social-Plus/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "fcaec1e2b27a40fff0c0",
+    "url": "/Social-Plus/static/css/main.9545a5bf.chunk.css"
+  },
+  {
+    "revision": "0f8e03ee711abb33482a",
+    "url": "/Social-Plus/static/js/2.aef2db72.chunk.js"
+  },
+  {
+    "revision": "174263e5b9d3e85c08d4329eaeeb7640",
+    "url": "/Social-Plus/static/js/2.aef2db72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcaec1e2b27a40fff0c0",
+    "url": "/Social-Plus/static/js/main.c69db030.chunk.js"
+  },
+  {
+    "revision": "8972cceac73fd7e6a9ab",
+    "url": "/Social-Plus/static/js/runtime-main.ac7da680.js"
+  },
+  {
+    "revision": "bc8cc4eae3d3822562e825fb5cec5536",
+    "url": "/Social-Plus/static/media/blue-marker.bc8cc4ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Social-Plus/static/media/logo.5d5d9eef.svg"
+  }
+]);
