bd21b51397c3361ac05243e8dd61c73099fcd4f4
黃韋豪 <huangwh@seas.upenn.edu>
2023-01-03 13:41:40 +0800

    change API_ROOT

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = "TOKEN_KEY";
-export const API_ROOT = "https://around-357613.uw.r.appspot.com";
+export const API_ROOT = "https://river-messenger-373603.de.r.appspot.com";
 
 export const GEO_OPTIONS = {
   enableHighAccuracy: true,
